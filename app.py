9f8df01c5ad7de644ff5ad74481e025da9d556bd
BiriBiri <constpi314@gmail.com>
2020-09-15 16:50:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-import lightgbm as lgb
-from sklearn.feature_extraction.text import TfidfVectorizer
-import numpy as np
-import pandas as pd
-import pickle
-
-
-
 from flask import Flask
 from flask import request
 import requests
@@ -19,12 +11,7 @@ import traceback
 application = Flask(__name__)
 
 
-#загружаем модели из файла
-vec = pickle.load(open("./models/tfidf.pickle", "rb"))
-model = lgb.Booster(model_file='./models/lgbm_model.txt')
 
-
-# тестовый вывод
 @application.route("/")  
 def hello():
     resp = {'message':"Hello World!"}
@@ -32,32 +19,6 @@ def hello():
     response = jsonify(resp)
     
     return response
-
-# предикт категории
-@application.route("/categoryPrediction" , methods=['GET', 'POST'])  
-def registration():
-    resp = {'message':'ok'
-           ,'category': -1
-           }
-
-    try:
-        getData = request.get_data()
-        json_params = json.loads(getData) 
-        
-        #напишите прогноз и верните его в ответе в параметре 'prediction'
-
-
-
-        
-    except Exception as e: 
-        print(e)
-        resp['message'] = e
-      
-    response = jsonify(resp)
-    
-    return response
-
-        
 
 if __name__ == "__main__":
     port = int(os.getenv('PORT', 5000))
