429301aadc5e3928720378c43579969f4dc71d16
BiriBiri <constpi314@gmail.com>
2020-09-16 10:25:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask import request
 import requests
 from flask import jsonify
-
+import pickle
+import pika
 import os
 import json
 from ast import literal_eval
@@ -10,7 +11,7 @@ import traceback
 
 application = Flask(__name__)
 
-
+model = pickle.load(open("./models/tfidf.pickle", "rb"))
 
 @application.route("/")  
 def hello():
@@ -20,9 +21,51 @@ def hello():
     
     return response
 
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host='localhost'))  # здесь нужно изменить, брать host из переменной окружения CLOUDAMQP_URL, которую мы добавим на хероку
+
+channel = connection.channel()
+
+exchange_name = 'model-exchange'
+queue_name = 'model-queue'
+routing_key = 'model-message'
+channel.exchange_declare(exchange=exchange_name, exchange_type='direct')
+channel.queue_declare(queue=queue_name)
+channel.queue_bind(queue=queue_name, exchange=exchange_name, routing_key=routing_key)
+
+
+def on_request(ch, method, props, body):
+    json_input = json.loads(body.decode("utf-8"))
+    result = some_function(json_input)
+    channel.basic_publish(
+        exchange=exchange_name,
+        routing_key='model-response'+'-'+json_input["RevenueForecastId"],
+        body=result.encode("utf-8"))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+def some_function(json_input):
+    print(json_input)
+    id = json_input["RevenueForecastId"]
+    print("inside function")
+    time.sleep(10)
+    print("sending")
+    return json.dumps({"RevenueForecastId": id, "Result": "123"})
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue=queue_name, on_message_callback=on_request)        
+
 if __name__ == "__main__":
+    Th = threading.Thread(channel.start_consuming())
+    Th.start()
+#    print(" [x] Awaiting RPC requests")
+#    channel.start_consuming()
+    
     port = int(os.getenv('PORT', 5000))
     application.run(debug=False, port=port, host='0.0.0.0' , threaded=True)
+
 
 
 
